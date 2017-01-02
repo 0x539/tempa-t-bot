73e7e731e5026f5dddfbc1e0e140d071ac83a944
Aaron -.- <omgbloodypass@gmail.com>
2017-01-02 04:41:41 +0000

    made the script that grabs the btc price send a message if it jumps or drops 10%

--- get_price.py
+++ get_price.py
@@ -1,7 +1,51 @@
+# coding=utf-8
+import os
 import urllib
 import json
+import requests
 
-data = json.loads(urllib.urlopen("https://api.coinbase.com/v2/prices/BTC-GBP/buy").read())
 
-with open('btc_price_cache.txt', 'w') as outfile:
-    json.dump(data, outfile)
+def get_cached_price():
+    with open(os.path.abspath(os.path.join("btc_price_cache.txt"))) as data_file:
+        data = json.load(data_file)
+
+    return float(data['data']['amount'])
+
+
+def get_new_price():
+    data = json.loads(urllib.urlopen("https://api.coinbase.com/v2/prices/BTC-GBP/buy").read())
+
+    with open('btc_price_cache.txt', 'w') as outfile:
+        json.dump(data, outfile)
+
+    return float(data['data']['amount'])
+
+
+def send_message(message):
+    _vars = {
+        'token': 'xoxb-122304064195-ZZpMGWCWGrOQq6Pz50aEZuz3',
+        'channel': 'G3M1V6G2K',
+        'text': message
+    }
+    url = 'https://slack.com/api/chat.postMessage?%s' % urllib.urlencode(_vars)
+    requests.post(url)
+
+
+def send_btc_update():
+    old = get_cached_price()
+    new = get_new_price()
+    diff = old - new
+    percent = (diff / old * 100) * -1
+    percent_string = "{:.2f}".format(percent)
+
+    info = "Was: £%s Now: £%s Diff: £%s (%s%s)" % (old, new, diff, percent_string, "%")
+
+    if percent > 10.0:
+        send_message("BTC is UP!")
+        send_message("%s" % info)
+    elif percent < -10.0:
+        send_message("BTC is DOWN!")
+        send_message("%s" % info)
+
+
+send_btc_update()
